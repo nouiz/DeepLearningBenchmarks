04d21bab47493a42b30b32f9738cf65da2091ddc
Frederic <nouiz@nouiz.org>
2012-03-19 14:52:01 -0400

    append to result file.

--- theano/run.sh
+++ theano/run.sh
@@ -16,24 +16,24 @@ MKL32_MLP='linker=cvm,device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -l
 MKL64_MLP='linker=cvm,device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
 GPU32_MLP='linker=cvm,device=gpu0,floatX=float32'
 
-(THEANO_FLAGS="$MKL32_MLP" python mlp.py 2>>./outs/errors.log > outs/${HOSTNAME}_mlp_cpu32.bmark) &&
-(THEANO_FLAGS="$MKL64_MLP" python mlp.py 2>>./outs/errors.log > outs/${HOSTNAME}_mlp_cpu64.bmark) && 
-(THEANO_FLAGS="$GPU32_MLP" python mlp.py 2>>./outs/errors.log > outs/${HOSTNAME}_mlp_gpu32.bmark) ; 
+(THEANO_FLAGS="$MKL32_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu32.bmark) &&
+(THEANO_FLAGS="$MKL64_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu64.bmark) &&
+(THEANO_FLAGS="$GPU32_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_gpu32.bmark) ;
 
-(THEANO_FLAGS="$MKL32" python convnet.py 2>>./outs/errors.log > outs/${HOSTNAME}_convnet_cpu32.bmark) && 
-(THEANO_FLAGS="$MKL64" python convnet.py 2>>./outs/errors.log > outs/${HOSTNAME}_convnet_cpu64.bmark) &&
-(THEANO_FLAGS="$GPU32" python convnet.py 2>>./outs/errors.log > outs/${HOSTNAME}_convnet_gpu32.bmark) ; 
+(THEANO_FLAGS="$MKL32" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu32.bmark) &&
+(THEANO_FLAGS="$MKL64" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu64.bmark) &&
+(THEANO_FLAGS="$GPU32" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_gpu32.bmark) ;
 
 
 cat /proc/cpuinfo |grep "model name"|uniq > ${HOSTNAME}_config.conf
 free >> ${HOSTNAME}_config.conf
 uname -a >>  ${HOSTNAME}_config.conf
 
-(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log > outs/${HOSTNAME}_rbm_cpu32_b1.bmark) &&
-(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log > outs/${HOSTNAME}_rbm_cpu32_b60.bmark) ;
+(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b1.bmark) &&
+(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b60.bmark) ;
 
-(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 1 100 2>>./outs/errors.log > outs/${HOSTNAME}_rbm_cpu64_b1.bmark) &&
-(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 60 20 2>>./outs/errors.log > outs/${HOSTNAME}_rbm_cpu64_b60.bmark) ;
+(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b1.bmark) &&
+(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b60.bmark) ;
 
-(THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log > outs/${HOSTNAME}_rbm_gpu32_b1.bmark) &&
-(THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log > outs${HOSTNAME}_rbm_gpu32_b60.bmark) ;
+(THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_gpu32_b1.bmark) &&
+(THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_gpu32_b60.bmark) ;
