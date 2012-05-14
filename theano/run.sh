7651cad05c1b7c21344fd819f695ccfbbce7c0b3
Frederic Bastien <nouiz@nouiz.org>
2012-05-14 14:58:14 -0400

    refactored the blas config.

--- theano/run.sh
+++ theano/run.sh
@@ -6,22 +6,23 @@
 # LIBRARY_PATH to include        /u/bergstrj/pub/intel/mkl/10.2.4.032/lib/em64t
 # THEANO_FLAGS="device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def -lpthread" python mlp.py
 
+BLAS='blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
 mkdir -p outs
-MKL32='linker=c|py_nogc,device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
-MKL64='linker=c|py_nogc,device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
-GPU32='linker=c|py_nogc,device=gpu0,floatX=float32'
+BLAS32='linker=c|py_nogc,device=cpu,floatX=float32',$BLAS
+BLAS64='linker=c|py_nogc,device=cpu,floatX=float64',$BLAS
+GPU32='linker=c|py_nogc,device=gpu0,floatX=float32,force_device=True'
 
 #MLP tests need the cvm linker.
-MKL32_MLP='linker=cvm,device=cpu,floatX=float32,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
-MKL64_MLP='linker=cvm,device=cpu,floatX=float64,blas.ldflags=-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lmkl_def'
-GPU32_MLP='linker=cvm,device=gpu0,floatX=float32'
+BLAS32_MLP='linker=cvm,device=cpu,floatX=float32',$BLAS
+BLAS64_MLP='linker=cvm,device=cpu,floatX=float64',$BLAS
+GPU32_MLP='linker=cvm,device=gpu0,floatX=float32,force_device=True'
 
-(THEANO_FLAGS="$MKL32_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu32.bmark) &&
-(THEANO_FLAGS="$MKL64_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu64.bmark) &&
+(THEANO_FLAGS="$BLAS32_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu32.bmark) &&
+(THEANO_FLAGS="$BLAS64_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_cpu64.bmark) &&
 (THEANO_FLAGS="$GPU32_MLP" python mlp.py 2>>./outs/errors.log >> outs/${HOSTNAME}_mlp_gpu32.bmark) ;
 
-(THEANO_FLAGS="$MKL32" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu32.bmark) &&
-(THEANO_FLAGS="$MKL64" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu64.bmark) &&
+(THEANO_FLAGS="$BLAS32" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu32.bmark) &&
+(THEANO_FLAGS="$BLAS64" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_cpu64.bmark) &&
 (THEANO_FLAGS="$GPU32" python convnet.py 2>>./outs/errors.log >> outs/${HOSTNAME}_convnet_gpu32.bmark) ;
 
 
@@ -30,11 +31,11 @@ free >> ${HOSTNAME}_config.conf
 uname -a >>  ${HOSTNAME}_config.conf
 w >> ${HOSTNAME}_config.conf
 
-(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b1.bmark) &&
-(THEANO_FLAGS="$MKL32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b60.bmark) ;
+(THEANO_FLAGS="$BLAS32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b1.bmark) &&
+(THEANO_FLAGS="$BLAS32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu32_b60.bmark) ;
 
-(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b1.bmark) &&
-(THEANO_FLAGS="$MKL64" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b60.bmark) ;
+(THEANO_FLAGS="$BLAS64" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b1.bmark) &&
+(THEANO_FLAGS="$BLAS64" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_cpu64_b60.bmark) ;
 
 (THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 1 100 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_gpu32_b1.bmark) &&
 (THEANO_FLAGS="$GPU32" python rbm.py 1024 1024 60 20 2>>./outs/errors.log >> outs/${HOSTNAME}_rbm_gpu32_b60.bmark) ;
